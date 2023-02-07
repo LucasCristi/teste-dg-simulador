08accb6937c9f6d0577288e55f14e0c9adb35f0e
Lucas Cristi <lucasccristi@gmail.com>
2023-02-07 13:43:49 -0300

    Finalizado

--- index.js
+++ index.js
@@ -5,25 +5,24 @@ calcular.addEventListener("click", () => {
     const taxa = parseInt(document.getElementById("taxa").value);
     const valorPago = parseInt(document.getElementById("valor-pago").value);
     const resultado = document.getElementById("resultado")
-    
-    const dividaTotal = (valorDoEmprestimo * (taxa/100) + valorDoEmprestimo).toFixed(3)
-    const valorAtualizadoAposPagamento = (dividaTotal - valorPago).toFixed(3)
-    const prazoCalculado = valorAtualizadoAposPagamento / valorPago
-    
+
     resultado.innerHTML = ""
 
-    for(i = 0; i <= prazoCalculado; i++){
-    
-        let mesesPagosAcumulados = valorPago * [i]
-        let dividaTotalAposPagamentos = dividaTotal - mesesPagosAcumulados
-        console.log(dividaTotalAposPagamentos)
+    let montante = valorDoEmprestimo
+    let ultimaParcela = 0
+    for (i = 0; montante >= 0; i++) {
+        montante = ((1 + (taxa / 100)) * montante - valorPago).toFixed(2)
+        console.log(montante)
 
-        if(dividaTotal > dividaTotalAposPagamentos){
-            dividaTotalAposPagamentos = dividaTotalAposPagamentos * taxa/100 * [i] + dividaTotalAposPagamentos
+        if (montante <= 0) {
+            ultimaParcela = -montante
+            console.log(ultimaParcela)
+            resultado.innerHTML += `<div class="infos-resultado">Mês ${[i + 1]} - Última parcela no valor de ${ultimaParcela}. Dívida encerrada!</div>`
+        } else {
+            resultado.innerHTML += `<div class="infos-resultado">Mês ${[i + 1]} - Pagando ${valorPago} você terá uma dívida atualizada de: R$ ${montante}</div>`
         }
-        
-        resultado.innerHTML += `<div class="infos-resultado">Mês ${[i+1]} - Pagando ${valorPago} você terá uma dívida atualizada de: R$ ${dividaTotalAposPagamentos}</div>`
-
     }
-   
+
+    resultado.innerHTML += `<div class="infos-resultado">Você pagou o total de R$ ${valorPago * [i] + ultimaParcela} </div>`
+
 });
